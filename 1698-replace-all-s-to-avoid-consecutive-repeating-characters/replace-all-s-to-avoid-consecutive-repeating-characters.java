5132daf829f71a90fce782d362842bf3bf53647b
Prithiv K <154400967+prithiv-k@users.noreply.github.com>
2024-07-05 12:05:21 +0530

    Time: 2 ms (23.68%) | Memory: 42.5 MB (22.93%) - LeetSync

---
+++ 1698-replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+class Solution {
+    public String modifyString(String s) {
+        Random rn = new Random();
+        StringBuilder ans = new StringBuilder();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '?') {
+                for (char c = 'a'; c <= 'z'; c++) {
+                    if ((i > 0 && c == ans.charAt(i - 1)) || (i < s.length() - 1 && c == s.charAt(i + 1))) {
+                        continue;
+                    }
+                    ans.append(c);
+                    break;
+                }
+            } else {
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+}
