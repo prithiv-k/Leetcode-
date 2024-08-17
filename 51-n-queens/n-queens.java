2c5f060095743772546d5fda28a25ccfd88f9b5d
Prithiv K <154400967+prithiv-k@users.noreply.github.com>
2024-08-17 23:04:36 +0530

    Time: 10 ms (14.94%) | Memory: 45 MB (16.07%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,51 +1,52 @@
 class Solution {
-    public boolean isSafe(int row,int col,int grid[][],int n){
-        for(int i=0;i<col;i++){
-            if(grid[row][i]==1) return false;
-        }
-       for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
-            if(grid[i][j]==1){
-                return false;
-            }
-        }
-        for(int i=row+1,j=col-1;i<n&&j>=0;i++,j--){
-            if(grid[i][j]==1){
-                return false;
-            }
-        }
-        return true;
-    }
-    public boolean solve(int col,int n,int grid[][],List<List<String>> ans){
-        if(col==n){
-            List<String> ar=new ArrayList<>();
-            for(int i=0;i<n;i++){
-                StringBuilder str=new StringBuilder("");
-                for(int j=0;j<n;j++){
-                    if(grid[i][j]==1)str.append("Q");
-                    else str.append(".");
-
-                }
-                ar.add(new String(str));
-            }
-            ans.add(new ArrayList<String>(ar));
-            return false;
-        }
-        for(int i=0;i<n;i++){
-            if(isSafe(i,col,grid,n)){
-                grid[i][col]=1;
-                if(solve(col+1,n,grid,ans)){
-                    return true;
-                }
-                grid[i][col]=0;
-            }
-        }
-        return false;
-
-    }
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans=new ArrayList<>();
-        int grid[][]=new int[n][n];
-        solve(0,n,grid,ans);
-        return ans;
+        List<List<String>> li=new ArrayList<>();
+        char b[][]=new char[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                b[i][j]='.';
+            }
+        }
+        solve(li,n,0,b);
+        return li;
+    }
+    static void solve(List<List<String>> li,int n,int c,char[][]b){
+        if(n==c){
+            List<String> h=new ArrayList<>();
+            for(int i=0;i<n;i++){
+                String s="";
+                for(int j=0;j<n;j++){
+                    s+=b[i][j];
+                }h.add(s);
+            }               
+            li.add(new ArrayList<>(h));
+
+
+        }else{
+            for(int row=0;row<n;row++){
+                if(isf(row,n,b,c)){
+                    b[row][c]='Q';
+                    solve(li,n,c+1,b);
+                    b[row][c]='.';
+                }
+            }
+        }
+    }
+    static boolean isf(int row,int n,char[][] b,int c){
+        for(int i=c-1;i>=0;i--){
+            if(b[row][i]=='Q'){
+                return false;
+            }
+        }
+            for(int i=row+1,j=c-1;i<n&&j>=0;i++,j--){
+                if(b[i][j]=='Q'){
+                    return false;
+                }
+            }for(int i=row-1,j=c-1;i>=0&&j>=0;i--,j--){
+                if(b[i][j]=='Q'){
+                    return false;
+                }
+            }
+        return true;
     }
 }
